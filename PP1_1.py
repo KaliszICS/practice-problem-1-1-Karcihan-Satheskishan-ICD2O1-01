eb16f47b99a255c8bdc7f3fb7467225d6ef00426
Karcihan-Satheskishan-ICD2O1-01 <karcihan.satheskishan@student.tdsb.on.ca>
2024-09-13 13:29:45 +0000

    Assignment 1: Print()

--- PP1_1.py
+++ PP1_1.py
@@ -1,16 +1,15 @@
 def q1():
   #Write Assignment code here
-
+  print("Hello World")
 def q2():
   #Write Assignment code here
-  
+  print(1)
+  print(2)
+  print(3)
+  print(4)
+  print(5)
 def q3():
   #Write Assignment code here
-  
-def q4:
+  print(" Help me!")
+def q4():
   #Write Assignment code here
-
-q1()
-q2()
-q3()
-q4()
